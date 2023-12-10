1dc2d4aa39654d7b171b26b0173d0a95369e6c99
matheus_miiller <matheusmiiller44@gmail.ccom>
2023-12-10 15:05:22 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+    document.getElementById("btn-verde").
+    addEventListener("click", function(){
+        document.querySelector("body").
+        setAttribute("class","verde")
+    })
+
+    document.getElementById("btn-amarelo").
+    addEventListener("click", function(){
+        document.querySelector("body").
+        setAttribute("class","amarelo")
+    })
+
+    document.getElementById("btn-azul").    
+    addEventListener("click", function(){
+        document.querySelector("body").
+        setAttribute("class","azul")
+    })
+
